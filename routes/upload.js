7dcdb5d1a8b18955b1fea58fbc45681949caa940
Kowndinya2000 <46968076+Kowndinya2000@users.noreply.github.com>
2020-07-09 00:28:28 +0530

    Final Release

--- routes/upload.js
+++ routes/upload.js
@@ -1,5 +1,6 @@
 var express = require('express')
-
+require('dotenv').config();
+const nodemailer = require('nodemailer');
 var GridFsStorage = require('multer-gridfs-storage')
 var path = require('path')
 var crypto = require('crypto')
@@ -41,7 +42,34 @@ file: (req,file)=>{
 const schedule = multer({storage})
 router.use(methodOverride('_method'))
 router.post('/',schedule.single('file'), function(req, res, next) {
-    res.redirect('/?rm=Event Added Successfully!')
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+          user: 'cs17b032@iittp.ac.in',
+          pass: 'I1t@googlechrome'
+      }
+  });
+
+  let mailOptions = {
+      from: 'cs17b032@iittp.ac.in',
+      to: req.body.email2,
+      subject:'An event lined up! [Title]: '+req.body.title + ' On ' + req.body.date + ' At: [' + req.body.time + ']' ,
+      html: '<!DOCTYPE html><html><head></head><body style="background-color: #f2f2f2;font-family: \'Lucida Sans\', \'Lucida Sans Regular\', \'Lucida Grande\', \'Lucida Sans Unicode\', Geneva, Verdana, sans-serif;"><div style="font-size: 2rem;text-align: center;background: #333333;font-family: \'Lucida Sans\', \'Lucida Sans Regular\', \'Lucida Grande\', \'Lucida Sans Unicode\', Geneva, Verdana, sans-serif;color: #456FFF;">Event Notification from Task-Out</div><h2 style="color: #7b7b7b">Hello ' + req.body.email2 +', this is<span style="color: #456FFF;">' + ' ' + 'You have an event scheduled! On ' + req.body.date + '</span></h2><h4 style="color: #7b7b7b">'+ '[Title]: ' + req.body.title +'</h4><h2 style="font-style: italic;">Links to Event Resources</h2><div style="border-left: 5px solid #456fff;border-radius: 1rem;border-right: 5px solid #456fff;padding: 0.5rem;margin-bottom: 1rem;"> ' + 'Link: [' + req.body.link  + ']' +  '</div><h2 style="font-style: italic;">Event Notes: </h2><div style="border-left: 5px solid #456fff;border-radius: 1rem;border-right: 5px solid #456fff;padding: 0.5rem;">' + req.body.note + '</div><h2 style="display: inline-block;"></a></h2><h2 style="display: inline-block;margin-left: 2rem;"><a href='+ req.body.link +' style="color:darkblue">Event Resource Link</a></a></h2></body></html>',
+  };
+  transporter.sendMail(mailOptions,function(err,data){
+      if(err)
+      {
+          console.log(err);
+          res.json({message: "Email not Sent :(" + err});
+
+      }
+      else{
+          console.log('Email sent successfully');
+          res.json({message: "Email Sent Successfully"});
+
+      }
+  });
+    res.redirect('/?rm=Event Added Successfully! An email has been sent!')
 });
 
 module.exports = router;
